993fa3280c8e6b999290b7bb23a2bf5420791427
Joseph Thayne <github@thaynefam.org>
2012-07-02 05:47:23 -0700

    Added some documentation

--- class.trimMysqli.php
+++ class.trimMysqli.php
@@ -1,14 +1,32 @@
 <?php
 
+/**
+ * stripMysqli - Extend PHP's mysqli_query function to remove unnecessary formatting from
+ *               the query in order to help reduce space used in logs as well as allow for
+ *               simpler log parsing.
+ *
+ * @author Joseph Thayne <webadmin@thaynefam.org>
+ */
 class stripMysqli extends mysqli
 {
     function query($query)
     {
+        /**
+         * Initialize variables
+         */
         $skipTillResetSingle = false;
         $skipTillResetDouble = false;
         $reachedFirst = false;
 
+        /**
+         * Remove any beginning or ending whitespace before continuing
+         */
         $query = trim($query);
+
+        /**
+         * Split the string into an array with a single key for each character
+         * The array will be looped through to find the extra formatting characters
+         */
         $charList = str_split($query);
 
         $queryLength = count($charList);
@@ -54,7 +72,6 @@ class stripMysqli extends mysqli
         }
 
         $query = implode('', $charList);
-
 
         $result = parent::query($query);
 
